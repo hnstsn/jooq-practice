324f859f303ea1b5c439bd97495c44f60e6ba331
WANSEO\hnsts <hnstsn@naver.com>
2024-07-10 22:18:03 +0900

    jooq 설정. db 접속 아이디, 비밀번호 주의

---
+++ README.md
@@ -0,0 +1,8 @@
+# jooq-practice
+jooq 공부
+
+spring boot 3.2.7
+jvm 17
+jooq 3.19.5
+
+인프런 실전 jOOQ! Type Safe SQL with Java 공부
---
+++ src/test/java/com/example/jooqpractice/JooqPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jooqpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JooqPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
